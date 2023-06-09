0fb4a7ec476b187f251a0d83c66efd86a2e461bb
Dmitrii Repnikov <dmitrii.amagkn@gmail.com>
2023-06-09 22:30:49 +0300

    fix html element exists assumption

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,20 @@
 import { faker } from "@faker-js/faker"
 
-let cartText = `<div>You have ${faker.number.int(10)} items in your cart</div>`
+const mount = (el) => {
+    let cartText = `<div>You have ${faker.number.int(10)} items in your cart</div>`
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+    el.innerHTML = cartText;
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-cart")
+
+    // We assume element with that ID exist only in products/public/index.html and not in container/public/index.html
+    // We can come up with more unique id for that
+    if (el) {
+        // We are probably running in isolation
+        mount(el)
+    }
+}
+
+export { mount }
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,28 @@
 import { faker } from "@faker-js/faker"
-let products = "";
 
-for(let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName()
+const mount = (el) => {
+    let products = "";
 
-    products += `<div>${name}</div>`
+    for(let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName()
+
+        products += `<div>${name}</div>`
+    }
+
+    el.innerHTML = products;
 }
 
-document.querySelector("#dev-product").innerHTML = products;
+// Running this frontend in development in isolation
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-product")
+
+    // We assume element with that ID exist only in products/public/index.html and not in container/public/index.html
+    // We can come up with more unique id for that
+    if (el) {
+        // We are probably running in isolation
+        mount(el)
+    }
+}
+
+// Export for running this file in development or production through the CONTAINER app
+export { mount }
